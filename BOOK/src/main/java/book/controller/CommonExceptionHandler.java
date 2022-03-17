97e5beace200b0a8ca16e62500d0b7751fbb6918
nnoonnight <101796435+nnoonnight@users.noreply.github.com>
2022-03-17 20:11:57 +0900

    Update CommonExceptionHandler.java

--- BOOK/src/main/java/book/controller/CommonExceptionHandler.java
+++ BOOK/src/main/java/book/controller/CommonExceptionHandler.java
@@ -3,6 +3,8 @@ package book.controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+//어디에 쓰이는지 모르겠음
+
 @ControllerAdvice("BOOK")
 public class CommonExceptionHandler {
 	@ExceptionHandler(Exception.class)
